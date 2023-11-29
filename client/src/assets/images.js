e8f0c8e29338c7b0c13263125da946f09650d412
Harsh Shah <hs553608@gmail.com>
2023-11-29 21:01:55 +0530

    Shopping Cart feature added and images.js

---
+++ client/src/assets/images.js
@@ -0,0 +1,6 @@
+import chalk from './images/chalk.png';
+import desk from './images/desk.png';
+import blackboard from './images/blackboard.png';
+
+
+export {chalk, desk, blackboard};
---
+++ client/src/components/context/shop-context.jsx
@@ -0,0 +1,91 @@
+import { createContext,ReactNode, useContext,useState ,useEffect} from "react";
+import { dataitems } from "../../../inventoryItems";
+
+export const ShopContext=createContext(null)
+
+const getDefaultCart = (inventory) => {
+  let cart = {};
+  inventory.forEach((item) => {
+    const itemId = item._id;
+    if (itemId !== undefined ) {
+      cart[itemId] = 0;
+    }
+  });
+  return cart;
+};
+
+
+export const ShopContextProvider=(props)=>{
+
+  const [inventory, setInventory] = useState([]);
+useEffect(() => {
+  // Fetch inventory data when the component mounts
+  fetchInventory();
+}, []);
+
+const fetchInventory = async () => {
+  try {
+    const response = await fetch('http://localhost:5000/allinventoryitems', {
+      method: 'GET',
+    });
+    const data = await response.json();
+    if (data.success) {
+      setInventory(data.data);
+      // console.log(inventory.length)
+    } else {
+      console.error('Failed to fetch inventory:', data.message);
+    }
+  } catch (error) {
+    console.error('Error fetching inventory:', error.message);
+  }
+};
+  const [cartItems,setCartItems]=useState(getDefaultCart(inventory))
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: parseInt(prev[itemId], 10) + 1 || 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: Math.max(parseInt(prev[itemId], 10) - 1, 0),
+    }));
+  };
+  const deleteFromCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: 0,
+    }));
+  };
+
+  const findTotal=()=>{
+    let total = 0;
+    for (const itemId in cartItems) {
+      if (cartItems.hasOwnProperty(itemId)) {
+        total += cartItems[itemId];
+      }
+    }
+    return total;
+  }
+
+  const findUnique=()=>{
+    const itemIds = Object.keys(cartItems);
+    // Count the number of unique items
+    const total = itemIds.filter(itemId => cartItems[itemId] > 0).length;
+    return total;
+  }
+
+   const contextValue={cartItems,addToCart,removeFromCart,deleteFromCart,findTotal,findUnique}
+  // console.log(JSON.stringify(cartItems))
+  console.log(findUnique())
+  
+  return(
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  )
+}
+
